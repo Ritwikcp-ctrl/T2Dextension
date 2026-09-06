40e37e79c8b2a9753f60db847529d2b666087460
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-09-06 23:41:58 +0530

    modified the logic

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,11 @@
 let totalKeyPressed = 0;
 
-const trackWindow = window.addEventListener("keypress", (key) => {
+const trackWindow = window.addEventListener("keypress", () => {
   totalKeyPressed++;
-  console.log(`Total keys pressed :${totalKeyPressed}, ${key.key}`);
+  //   console.log(`Total keys pressed :${totalKeyPressed}, ${key.key}`);
 
   function isMultipleof(totalKeyPressed) {
-    if (totalKeyPressed % 100 === 0 && totalKeyPressed != 0) {
+    if (totalKeyPressed) {
       console.log(`Distance travelled is : ${totalKeyPressed / 100} m`);
     }
   }
