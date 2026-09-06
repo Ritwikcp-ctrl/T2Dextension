9912f61132b750b57d48168a31d6bf0f0717f63f
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-09-06 14:16:08 +0530

    actual logic

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,14 @@
-window.addEventListener("keypress", (event)=> {
-    console.log(`key pressed down: ${event.key}`)
-})
+let totalKeyPressed = 0;
+
+const trackWindow = window.addEventListener("keypress", (key) => {
+  totalKeyPressed++;
+  console.log(`Total keys pressed :${totalKeyPressed}, ${key.key}`);
+
+  function isMultipleof(totalKeyPressed) {
+    if (totalKeyPressed % 100 === 0 && totalKeyPressed != 0) {
+      console.log(`Distance travelled is : ${totalKeyPressed / 100} m`);
+    }
+  }
+
+  isMultipleof(totalKeyPressed);
+});
