f067447caeeb02fd0f00286092b992cbf59963cd
Ritwikcp-ctrl <su30rai213@gmail.com>
2026-09-07 18:30:19 +0530

    modified the files

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,74 @@
-console.log("POPUP JS RUNNING");
+const distanceElement =
+document.getElementById("distance");
 
-chrome.storage.local.get("keyCount").then((result) => {
+const distanceStatElement =
+document.getElementById("distanceStat");
 
-  console.log("Storage result:", result);
+const keyCountElement =
+document.getElementById("keyCount");
 
-  const keyCount = result.keyCount || 0;
-  const distance = keyCount / 100;
+const messageElement =
+document.getElementById("message");
 
-  document.getElementById("keys").textContent =
-    `Keys typed: ${keyCount}`;
+const resetButton =
+document.getElementById("reset");
 
-  document.getElementById("distance").textContent =
-    `Distance: ${distance.toFixed(2)} m`;
+
+function updateMessage(distance){
+
+    if(distance===0){
+
+        return "Start typing to begin your journey.";
+
+    }else if(distance<1){
+
+        return "You're on your way.";
+
+    }else if(distance<10){
+
+        return "You've typed the distance of a short walk.";
+
+    }else{
+
+        return "That's serious keyboard mileage.";
+    }
+}
+
+
+async function updateUI(){
+
+    const result =
+    await chrome.storage.local.get("keyCount");
+
+    const keyCount =
+    result.keyCount || 0;
+
+    const distance =
+    keyCount/100;
+
+    distanceElement.textContent =
+    distance.toFixed(2);
+
+    distanceStatElement.textContent =
+    `${distance.toFixed(2)} m`;
+
+    keyCountElement.textContent =
+    keyCount.toLocaleString();
+
+    messageElement.textContent =
+    updateMessage(distance);
+}
+
+
+resetButton.addEventListener(
+"click",
+async ()=>{
+
+    await chrome.storage.local.set({
+        keyCount:0
+    });
+
+    updateUI();
 });
+
+updateUI();
